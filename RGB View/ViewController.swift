3065b6462bd7f44564c804b7e67adc000d5ad4b8
NosovaDaria <kuleshovadasha@gmail.com>
2021-12-12 04:16:46 +0300

    Setup Main and View Controller

--- RGB View/ViewController.swift
+++ RGB View/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+ 
+  // MARK: - IB Outlets
+  @IBOutlet var coloredView: UIView!
+  
+  @IBOutlet var redValueLabel: UILabel!
+  @IBOutlet var greenValueLabel: UILabel!
+  @IBOutlet var blueValueLabel: UILabel!
+  
+  @IBOutlet var redSlider: UISlider!
+  @IBOutlet var greenSlider: UISlider!
+  @IBOutlet var blueSlider: UISlider!
+  
+  // MARK: - Life Cycles Methods
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    coloredView.layer.cornerRadius = 32
+    
+    redSlider.value = 0
+    greenSlider.value = 0
+    blueSlider.value = 0
+    
+    redValueLabel.text = String(redSlider.value)
+    greenValueLabel.text = String(greenSlider.value)
+    blueValueLabel.text = String(blueSlider.value)
+    
+    redSlider.minimumTrackTintColor = .red
+    greenSlider.minimumTrackTintColor = .green
+    blueSlider.minimumTrackTintColor = .blue
   }
+  
 
-
+  @IBAction func slidersAction() {
+    let redSliderValue = CGFloat(redSlider.value)
+    let greenSliderValue = CGFloat(greenSlider.value)
+    let blueSliderValue = CGFloat(blueSlider.value)
+    let alphaValue = CGFloat(redSliderValue + greenSliderValue + blueSliderValue)
+    
+    coloredView.backgroundColor = UIColor(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue, alpha: alphaValue)
+    
+    redValueLabel.text = String(round(redSlider.value * 100) / 100)
+    greenValueLabel.text = String(round(greenSlider.value * 100) / 100)
+    blueValueLabel.text = String(round(blueSlider.value * 100) / 100)
+    
+  }
+  
 }
-
